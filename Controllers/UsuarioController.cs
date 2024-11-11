351ea6926f092c6545bcda85d5082806491afdaa
Angel04 <angeldelossantosrodriguez04@gmail.com>
2024-11-11 11:25:12 -0400

    ultimo commit,  se agregan la clase user y el controlloer usuario etc, ahora si funcional

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Empresa_A.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+       public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+    }
+}
