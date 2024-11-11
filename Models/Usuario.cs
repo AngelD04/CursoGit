b0abb4d5fce3cdb50cdbddc8698ade4cd120d778
AngelD04 <angeldelossantosrodriguez04@gmail.com>
2024-11-11 15:45:01 -0400

    Update from hub Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -4,6 +4,10 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+    
         public int Edad { get; set; }
+
+            public string telefono { get; set; }
+        public int salario { get; set; }
     }
 }
